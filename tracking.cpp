b6f54cf8e2f62ece71b76c669d57b30e1df23fd9
shohei-inoue <130023216+shohei-inoue@users.noreply.github.com>
2023-11-22 16:05:32 +0900

    Add files via upload

--- tracking.cpp
+++ tracking.cpp
@@ -1,10 +1,10 @@
-# define _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
 #define _USE_MATH_DEFINES
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <cmath>
-#include<opencv2/opencv.hpp>
+#include <opencv2/opencv.hpp>
 #include <vector>
 #include <chrono>
 using namespace std;
@@ -15,9 +15,8 @@ string source_video = "./videos/source.mp4";
 string coverage_ratio_pass = "./csv/coverage_ratio.csv";
 string source = "source";
 string heatmap = "heatmap";
-const int threshold = 75;  // R画素の差分差の閾値
 const int camera_type = 1; // pcカメラ->0, 外部カメラ->1
- // const int color_bar_th = 10;
+// const int color_bar_th = 10;
 /* ------------------- struct -----------------------------*/
 struct mouseParam
 {
@@ -26,12 +25,16 @@ struct mouseParam
   int event;
   int flags;
 };
+
+struct Robot {
+  cv::Ptr<cv::Tracker> tracker;
+  vector<cv::Rect> boundingBoxes;
+};
 /* ------------------- function header --------------------*/
 bool IsPointInPolygon(int x, int y, const vector<pair<int, int>> &p);
 void CallBack_mouse(int eventType, int x, int y, int flags, void *userdata);
-
-/* ------------------- main --------------------*/
-int main(int argc, char **argv)
+    /* ------------------- main --------------------*/
+    int main(int argc, char **argv)
 {
   cv::VideoCapture cap_in;
   cv::VideoWriter cap_out, cap_out2;
@@ -43,33 +46,47 @@ int main(int argc, char **argv)
   vector<pair<int, int>> p;
   mouseParam mouseEvent;
   int p_counter = 0;
+  vector<Robot> robots;
 
   cv::namedWindow(source, cv::WINDOW_AUTOSIZE);
 
   /* プログラム起動時に何も入力しなければカメラが, 動画ファイルのパスを入力すればその動画をソースとする */
-  if (argc == 1) {
+  if (argc == 1)
+  {
     cap_in.open(camera_type);
-  } else {
+  }
+  else
+  {
     cap_in.open(argv[1]);
   }
 
-  if (!cap_in.isOpened()) {
+  if (!cap_in.isOpened())
+  {
     cerr << "Couldn't open capture." << endl;
     return -1;
   }
 
   // 網羅率出力用ファイル
   ofstream csv_coverage_ratio(coverage_ratio_pass);
-  if (!csv_coverage_ratio) {
+  if (!csv_coverage_ratio)
+  {
     cout << "Couldn't open csv file." << endl;
     return -1;
-  } else {
+  }
+  else
+  {
     cout << "open csv file." << endl;
     csv_coverage_ratio << "time" << ",";
     csv_coverage_ratio << "coverage_ratio" << endl;
   }
 
   // 点の取得
+  cap_in >> frame_in;
+  if (frame_in.empty()) {
+      cout << "Error! source video frame empty." << endl;
+      break;
+    }
+  
   while (1) {
     cv::waitKey(100);
     cap_in >> frame_in;
@@ -92,11 +109,13 @@ int main(int argc, char **argv)
   }
 
   // 録画後解析用処理
-  if (argc != 1) {
+  if (argc != 1)
+  {
     cap_in.release();
 
     cap_in.open(argv[1]);
-    if (!cap_in.isOpened()) {
+    if (!cap_in.isOpened())
+    {
       cerr << "Couldn't open capture." << endl;
       return -1;
     }
@@ -107,13 +126,12 @@ int main(int argc, char **argv)
   cap_fps = cap_in.get(cv::CAP_PROP_FPS);
 
   // カウンター用のcv::Mat配列 Rをカウンターとして使用
-  cv::Mat counter = cv::Mat::zeros(cv::Size(cap_width, cap_height), CV_8UC3);
+  cv::Mat counter = cv::Mat::zeros(cv::Size(cap_width, cap_height), CV_32S);
 
   cv::namedWindow(heatmap, cv::WINDOW_AUTOSIZE);
   cap_out.open(heatmap_video, cv::VideoWriter::fourcc('m', 'p', '4', 'v'), cap_fps, cv::Size(cap_width, cap_height), true);
   cap_out2.open(source_video, cv::VideoWriter::fourcc('m', 'p', '4', 'v'), cap_fps, cv::Size(cap_width, cap_height), true);
 
-  
   // 動画の撮影開始時間を記録
   auto start_time = chrono::high_resolution_clock::now();
 
@@ -125,67 +143,83 @@ int main(int argc, char **argv)
       break;
     }
 
+    for (auto &robot : robots) {
+      cv::Rect boundingBox;
+      bool success = robot.tracker->update(frame_in, boundingBox);
+
+      if (success) {
+        robot.boundingBoxes.push_back(boundingBox);
+
+        // 通過した画素をカウント
+        for (int y = boundingBox.y; y < boundingBox.y + boundingBox.height; y++) {
+          for (int x = boundingBox.x; x < boundingBox.x + boundingBox.width; x++) {
+            if (x >= 0 && x < cap_width && y >= 0 && y < cap_height) {
+              counter.at<int>(y, x)++;
+            } // if 
+          } // for x
+        }  // for y
+      } else {
+        cv::putText(frame_hm, "Tracking failure detected!", cv::Point(100, 80), cv::FONT_HERSHEY_SIMPLEX, 0.75, cv::Scalar(0, 0, 255), 2);
+      }
+    } // for (auto &robot : robots)
+
     cv::Mat frame_hm = cv::Mat::zeros(cv::Size(cap_width, cap_height), CV_8UC3);
 
     int explored_counter = 0;
     if (cou != 0) {
-      for (int i = 0; i < frame_in.rows - 1; i++) {
-        for (int j = 0; j < frame_in.cols - 1; j++) {
-          if ((frame_in.at<cv::Vec3b>(i, j)[2] - frame_in_bef.at<cv::Vec3b>(i, j)[2]) >= threshold) {
-            counter.at<cv::Vec3b>(i, j)[2]++;
-          } // if threshold
-
-          if (IsPointInPolygon(j, i, p) == true) {
+      for (int y = 0; y < frame_in.rows - 1; y++) {
+        for (int x = 0 ; x < frame_in.cols - 1; x++) {
+          if (IsPointInPolygon(x, y, p) == true) {
             if (cou == 1) {
               exploration_counter++;
             }
-            switch (counter.at<cv::Vec3b>(i, j)[2]) {
-            case 0:
-              frame_hm.at<cv::Vec3b>(i, j) = cv::Vec3b(255, 0, 0);
-              break;
-            case 1:
-              frame_hm.at<cv::Vec3b>(i, j) = cv::Vec3b(153, 101, 0);
-              break;
-            case 2:
-              frame_hm.at<cv::Vec3b>(i, j) = cv::Vec3b(120, 130, 0);
-              break;
-            case 3:
-              frame_hm.at<cv::Vec3b>(i, j) = cv::Vec3b(101, 153, 0);
-              break;
-            case 4:
-              frame_hm.at<cv::Vec3b>(i, j) = cv::Vec3b(50, 204, 0);
-              break;
-            case 5:
-              frame_hm.at<cv::Vec3b>(i, j) = cv::Vec3b(0, 255, 0);
-              break;
-            case 6:
-              frame_hm.at<cv::Vec3b>(i, j) = cv::Vec3b(0, 203, 51);
-              break;
-            case 7:
-              frame_hm.at<cv::Vec3b>(i, j) = cv::Vec3b(0, 152, 102);
-              break;
-            case 8:
-              frame_hm.at<cv::Vec3b>(i, j) = cv::Vec3b(0, 101, 153);
-              break;
-            case 9:
-              frame_hm.at<cv::Vec3b>(i, j) = cv::Vec3b(0, 51, 203);
-              break;
-            default:
-              frame_hm.at<cv::Vec3b>(i, j) = cv::Vec3b(0, 0, 255);
-              break;
-            }
 
-            if (frame_hm.at<cv::Vec3b>(i, j)[0] != 255) {
-              explored_counter++; // 1回通った位置を探査済みピクセルとする
+            switch (counter.at<int>(y, x)) {
+              case 0:
+                frame_hm.at<cv::Vec3b>(y, x) = cv::Vec3b(255, 0, 0);
+                break;
+              case 1:
+                frame_hm.at<cv::Vec3b>(y, x) = cv::Vec3b(153, 101, 0);
+                break;
+              case 2:
+                frame_hm.at<cv::Vec3b>(y, x) = cv::Vec3b(120, 130, 0);
+                break;
+              case 3:
+                frame_hm.at<cv::Vec3b>(y, x) = cv::Vec3b(101, 153, 0);
+                break;
+              case 4:
+                frame_hm.at<cv::Vec3b>(y, x) = cv::Vec3b(50, 204, 0);
+                break;
+              case 5:
+                frame_hm.at<cv::Vec3b>(y, x) = cv::Vec3b(0, 255, 0);
+                break;
+              case 6:
+                frame_hm.at<cv::Vec3b>(y, x) = cv::Vec3b(0, 203, 51);
+                break;
+              case 7:
+                frame_hm.at<cv::Vec3b>(y, x) = cv::Vec3b(0, 152, 102);
+                break;
+              case 8:
+                frame_hm.at<cv::Vec3b>(y, x) = cv::Vec3b(0, 101, 153);
+                break;
+              case 9:
+                frame_hm.at<cv::Vec3b>(y, x) = cv::Vec3b(0, 51, 203);
+                break;
+              default:
+                frame_hm.at<cv::Vec3b>(y, x) = cv::Vec3b(0, 0, 255);
+                break;
+            } //switch
+
+            if (frame_hm.at<cv::Vec3b>(y, x)[0] != 255) {
+              explored_counter++;
             }
-          }
-          else
-          {
-            frame_hm.at<cv::Vec3b>(i, j) = frame_in.at<cv::Vec3b>(i, j);
-          } // if area_def_x, area_def_y == true
-        }   // for j
-      }     // for i
-    }       // if cou != 0
+          } else {
+            frame_hm.at<cv::Vec3b>(y, x) = frame_in.at<cv::Vec3b>(y, x);
+          } // IsPointInPolygon(x, y, p) == true
+        } // for x
+      } // for y
+    } // if cou != 0
+    
 
     cv::imshow(source, frame_in);
     cv::imshow(heatmap, frame_hm);
@@ -194,29 +228,26 @@ int main(int argc, char **argv)
     cap_out2 << frame_in;
 
     frame_in_bef = frame_in.clone();
-    
+
     // 動画内の経過時間を算出
     
     auto end_time = chrono::high_resolution_clock::now();
     auto elapsed_time = chrono::duration_cast<chrono::milliseconds>(end_time - start_time).count();
     
+    
     if (argc != 1) {
       elapsed_time = cou / cap_fps;
     }
-    
-    
 
     // 網羅率を算出
     double area_coverage_ratio = double(explored_counter) / double(exploration_counter) * 100.0;
     cout << "area coverage ratio : " << area_coverage_ratio << endl;
 
     // csvファイルへの書き込み
-    
     csv_coverage_ratio << elapsed_time << ",";
     csv_coverage_ratio << area_coverage_ratio << endl;
 
-    if (cv::waitKey(1) == 27)
-    { // ESC -> end video
+    if (cv::waitKey(1) == 27) { // ESC -> end video
       break;
     }
   } // for cou = 0; ; cou++
